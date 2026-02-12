9affb2559e270705099cb76d8538a9cfdeeb1fff
gabri <gabriele.amorosi@outlook.com>
2026-02-12 12:15:52 +0100

    removed the update thingy

---
+++ app/src/Config.php
@@ -0,0 +1,10 @@
+<?php
+namespace App;
+
+class Config {
+    // Azure SQL Server host
+    public const DB_SERVER_NAME = 'mysql';
+    public const DB_USERNAME = 'root';          // sometimes YOURUSER@YOURSERVER
+    public const DB_PASSWORD = 'secret123';
+    public const DB_NAME = 'name';
+}
