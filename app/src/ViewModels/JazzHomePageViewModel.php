e6cb706fac7cb3d4ff21e2c3d5914965da428b6d
gabri <gabriele.amorosi@outlook.com>
2026-03-04 11:48:37 +0100

    adding viewmodel for jazzhomepage

---
+++ app/src/ViewModels/JazzHomePageViewModel.php
@@ -0,0 +1,14 @@
+<?php
+// app/src/ViewModel/JazzHomePageViewModel.php
+
+declare(strict_types=1);
+
+namespace App\ViewModel;
+
+final class JazzHomePageViewModel
+{
+    public function __construct(
+        public readonly array $content,  // decoded JSON from Page.Content
+        public readonly array $events    // normalized jazz events
+    ) {}
+}
